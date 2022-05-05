141dd23dca6d8d469e81a14be5f3a849fdab04a7
Lucas Souza <lucasp.sdev@gmail.com>
2022-05-05 20:05:28 -0300

    feat: computed props

---
+++ src/components/NumberRender/NumberRender.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>{{ setEvenNumber }}</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NumberRender",
+  props: {
+    even: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    setEvenNumber() {
+      const evens = [];
+      const odds = [];
+
+      for (let i = 1; i < 10; i++) {
+        if (i % 2 === 0) {
+          evens.push(i);
+        } else {
+          odds.push(i);
+        }
+      }
+
+      return this.even === true ? evens.join(", ") : odds.join(", ");
+    },
+  },
+});
+</script>
